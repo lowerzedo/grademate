a7f2de5c8ebe35e6a3b93d7f5cfadf516248260a
Nazar Hasanov <nazarhasanov@ip-10-111-65-173.ap-southeast-1.compute.internal>
2024-06-19 16:10:12 +0800

    fix sem

--- app/services/subject.py
+++ app/services/subject.py
@@ -21,33 +21,37 @@ def get_all_subjects(**kwargs):
     return jsonify(subjects_serialize), 200
 
 def student_class_code(**kwargs):
-
     student_id = request.args.get("student_id")
 
-    student_exist = db.session.execute(select(Student).where(Student.student_id == student_id)).scalars().first()
+    # Check if the student exists
+    student_exist = db.session.execute(
+        select(Student).where(Student.student_id == student_id)
+    ).scalars().first()
 
     if not student_exist:
-        return jsonify({"message":"Student doesn't exist"}), 404
+        return jsonify({"message": "Student doesn't exist"}), 404
 
+    # Query to fetch subjects for the student's current semester and program
     stmt = (
         select(Subject)
         .join(Semester, Subject.semester_id == Semester.semester_id)
-        .join(Student, Semester.semester_id == Student.current_semester)
         .join(Program, Semester.program_id == Program.program_id)
-        .where(Program.program_id == student_exist.program_id)
+        .where(
+            (Student.student_id == student_id) &
+            (Student.current_semester == Subject.semester_id) &
+            (Program.program_id == student_exist.program_id)
+        )
         .distinct()
     )
 
     student_class_codes = db.session.execute(stmt).scalars().all()
 
-
     if not student_class_codes:
         return jsonify([]), 200
 
-
+    # Serialize the results
     student_class_codes_serialized = [class_code.serialize() for class_code in student_class_codes]
 
-    
     return jsonify(student_class_codes_serialized), 200
 
 
